db3909baec606b28b5173497b6456aa4171dc23c
maria-walker <mariawalker5@ymail.com>
2021-06-21 11:47:03 +0100

    removed comments from app.js

--- app.js
+++ app.js
@@ -5,24 +5,16 @@ const {
   handlePSQL400Errors,
   send404,
 } = require("./errors/errors");
-//const { getTopics } = require("./controllers/topics.controllers");
+
 const apiRouter = require("./routes/api.router");
 
-//const {errors} = require('./errors/errors')
-
 const app = express();
-
-//standard middleware
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
 
-//app.get("/api/topics", getTopics);
-
 app.all("*", send404);
-
-// //error-handling middleware
 
 app.use(handleCustomErrors);
 app.use(handlePSQL400Errors);
